1e935090265bc18ea0b9131e0eee8cd070543773
Ahmad Ali <xevenahmad@gmail.com>
2024-10-10 12:54:52 +0500

    change in button package

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import MyButtonPkg from '../../index'
-// import "dist/style.css"
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import ButtonComponent from '../../src';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      {/* <h4>Hello</h4> */}
-      <MyButtonPkg value="Click Here" variant="secondary" size="large" type='submit' role="submit"/>
+      {/* Pass props like `value`, `variant`, and `size` to the ButtonComponent */}
+      <ButtonComponent
+        value="Click Ahmad"
+        variant="primary"  // You can choose between "primary", "secondary", "danger"
+        size="medium"      // You can choose between "small", "medium", "large"
+        onClick={() => setCount(count + 1)}
+      />
+      <p>Count: {count}</p>
     </>
   );
 }
